0b913cdeea9fa4b671a7263127e0e738fe68105f
George Pantazis <george.pantazis@vodafoneinnovus.com>
2023-04-12 14:07:40 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module subscan
+
+go 1.20
+
+require (
+	github.com/jawher/mow.cli v1.2.0
+	github.com/miekg/dns v1.1.53
+	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e
+)
+
+require (
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/tools v0.3.0 // indirect
+)
---
+++ subscan.go
@@ -0,0 +1,230 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	cli "github.com/jawher/mow.cli"
+	"github.com/miekg/dns"
+	"github.com/tatsushid/go-fastping"
+)
+
+type Output struct {
+	ScanDate  string       `json:"scanDate"`
+	Subnet    string       `json:"subnet"`
+	HostIP    string       `json:"hostIP"`
+	DNSServer string       `json:"dnsServer"`
+	Results   []ScanResult `json:"results"`
+}
+
+type ScanResult struct {
+	DateTime  string   `json:"dateTime"`
+	HostIP    string   `json:"hostIP"`
+	TargetIP  string   `json:"targetIP"`
+	OpenPorts []int    `json:"openPorts"`
+	PTRRecord string   `json:"ptrRecord"`
+	ARecords  []string `json:"aRecords"`
+	CNAMEs    []string `json:"cnames"`
+	IPMatch   bool     `json:"ipMatch"`
+	TTL       int      `json:"ttl"`
+}
+
+var (
+	appversion string = "1.0.0"
+	outputFile string
+)
+
+func main() {
+	app := cli.App("subscan", "Generate a JSON file with all the responding hosts in a subnet")
+	app.Spec = "SUBNET [-d | --dns]"
+	app.Version("v version", appversion)
+
+	subnet := app.StringArg("SUBNET", "", "Subnet to scan, example 192.168.178.0/24")
+	dnsServer := app.StringOpt("d dns", "192.168.178.7:53", "DNS Server to Use, example 192.168.178.7:53")
+
+	app.Action = func() {
+		localIP := getLocalIP()
+
+		currentDate := time.Now().Format("2006-01-02")
+		outputFilename := fmt.Sprintf("%s-%s-scan.json", strings.ReplaceAll(*subnet, "/", "-"), currentDate)
+
+		ips, _ := getIPsFromSubnet(*subnet)
+		results := make([]ScanResult, 0)
+
+		var wg sync.WaitGroup
+		for _, ip := range ips {
+			wg.Add(1)
+			go func(ip string) {
+				defer wg.Done()
+				result := ScanResult{
+					DateTime: time.Now().Format(time.RFC3339),
+					HostIP:   localIP,
+					TargetIP: ip,
+				}
+
+				timeout := 1 * time.Second
+				if isUp, ttl := tcpPing(ip, timeout); isUp {
+					fmt.Printf("TTL for %s: %d ms\n", ip, ttl)
+					result.OpenPorts = scanPorts(ip, []int{80, 443, 22})
+					result.PTRRecord, result.ARecords, result.CNAMEs = getDNSRecords(ip, *dnsServer)
+					result.IPMatch = checkIPMatch(ip, result.ARecords)
+					result.TTL = ttl
+					results = append(results, result)
+				}
+			}(ip)
+		}
+
+		wg.Wait()
+
+		output := Output{
+			ScanDate:  time.Now().Format(time.RFC3339),
+			Subnet:    *subnet,
+			HostIP:    localIP,
+			DNSServer: *dnsServer,
+			Results:   results,
+		}
+
+		jsonData, _ := json.MarshalIndent(output, "", "  ")
+		err := os.WriteFile(outputFilename, jsonData, 0644)
+		if err != nil {
+			fmt.Println("Error writing JSON file:", err)
+			return
+		}
+
+		fmt.Println("Scan completed, results saved to ", outputFilename)
+	}
+	app.Run(os.Args)
+}
+
+func getIPsFromSubnet(subnet string) ([]string, error) {
+	ip, ipnet, err := net.ParseCIDR(subnet)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+
+	if len(ips) > 0 {
+		ips = ips[1 : len(ips)-1]
+	}
+	return ips, nil
+}
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
+
+func tcpPing(ip string, timeout time.Duration) (bool, int) {
+	p := fastping.NewPinger()
+	ra, err := net.ResolveIPAddr("ip4:icmp", ip)
+	if err != nil {
+		fmt.Println(err)
+		return false, 0
+	}
+	p.AddIPAddr(ra)
+
+	var isUp bool
+	var ttl int
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		isUp = true
+		ttl = int(rtt / time.Millisecond)
+	}
+
+	err = p.Run()
+	if err != nil {
+		fmt.Println(err)
+		return false, 0
+	}
+
+	return isUp, ttl
+}
+
+func scanPorts(ip string, ports []int) []int {
+	var openPorts []int
+	for _, port := range ports {
+		timeout := time.Second
+		conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", ip, port), timeout)
+		if err == nil {
+			openPorts = append(openPorts, port)
+			conn.Close()
+		}
+	}
+	return openPorts
+}
+
+func getDNSRecords(ip, dnsServer string) (string, []string, []string) {
+	var ptrRecord string
+	var aRecords []string
+	var cnames []string
+
+	reverseIP, err := dns.ReverseAddr(ip)
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", nil, nil
+	}
+	ptrMsg := dns.Msg{}
+	ptrMsg.SetQuestion(reverseIP, dns.TypePTR)
+	ptrResponse, err := dns.Exchange(&ptrMsg, dnsServer)
+
+	if err == nil {
+		for _, ans := range ptrResponse.Answer {
+			if ptr, ok := ans.(*dns.PTR); ok {
+				ptrRecord = ptr.Ptr
+				break
+			}
+		}
+	}
+
+	if ptrRecord != "" {
+		aMsg := dns.Msg{}
+		aMsg.SetQuestion(dns.Fqdn(ptrRecord), dns.TypeA)
+		aResponse, err := dns.Exchange(&aMsg, dnsServer)
+
+		if err == nil {
+			for _, ans := range aResponse.Answer {
+				if aRecord, ok := ans.(*dns.A); ok {
+					aRecords = append(aRecords, aRecord.A.String())
+				}
+				if cname, ok := ans.(*dns.CNAME); ok {
+					cnames = append(cnames, cname.Target)
+				}
+			}
+		}
+	}
+
+	return ptrRecord, aRecords, cnames
+}
+
+func checkIPMatch(targetIP string, aRecords []string) bool {
+	for _, aRecord := range aRecords {
+		if targetIP == aRecord {
+			return true
+		}
+	}
+	return false
+}
+
+func getLocalIP() string {
+	conn, err := net.Dial("udp", "192.168.178.7:80")
+	if err != nil {
+		return ""
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP.String()
+}
